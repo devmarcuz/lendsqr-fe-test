6ebbf59d2eec0014b497faeed813e36f5c3569b9
devmarcuz <otunbamarcusademola@gmail.com>
2023-07-13 00:41:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <h1>hello world</h1>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
