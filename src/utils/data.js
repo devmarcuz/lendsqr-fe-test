ec38c412bb00c8266f9d6acb85644b254179ffb0
devmarcuz <otunbamarcusademola@gmail.com>
2023-07-16 16:22:29 +0100

    cleaned

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,10 +1,10 @@
 exports.shortenEmail = (email) => {
   if (email.length <= 8) {
-    return email; // Return the email as is if its length is not more than 20 characters
+    return email;
   }
 
-  const username = email.substring(0, 10); // Take the first 10 characters of the email username
-  const domain = email.substring(email.indexOf("@")); // Take the domain part of the email
+  const username = email.substring(0, 10);
+  const domain = email.substring(email.indexOf("@"));
 
   return `${username}...${domain}`;
 };
